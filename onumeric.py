8c050f95963cf09a32ff5c132b052e9e32bfbc6a
yasskad <31750396+yasskad@users.noreply.github.com>
2018-04-15 22:53:52 -0400

    onumeric.py method with one gaussian added

--- onumeric.py
+++ onumeric.py
@@ -7,16 +7,16 @@ from pyspark.mllib.linalg import Vectors
 from pyspark.mllib.linalg import DenseMatrix
 
 
-def find_outliers_KMeans(sc, sequence,k=2,proportion=0.95):
+def find_outliers_KMeans(sequence,k=2,proportion=0.95):
 	#currently please take input as a list and return a list
         #for now, k=2
-        df=sc.parallelize(sequence)
+        df=sequence
         df_vector=df.map(lambda x: np.array(float(x)))
         
-        clusters=KMeans.train(df_vector, maxIterations=10,initializationMode="random")
+        clusters=KMeans.train(df_vector, k, maxIterations=10,initializationMode="random")
         center=clusters.centers
 
-        l=df_vector.collect()
+        l=np.array(df_vector.collect())
         l0=abs(l-center[0])
         l1=abs(l-center[1])
 
@@ -31,15 +31,16 @@ def find_outliers_KMeans(sc, sequence,k=2,proportion=0.95):
         else:
                 return []
 
-def find_outliers_KGuaussians(sc, sequence,k=2,proportion=0.95):
+def find_outliers_KGuaussians(sequence,k=2,proportion=0.95):
         #currently please take input as a list and return a list
         #for now, k=2
-        df=sc.parallelize(sequence)
+        df=sequence
         df_vector=df.map(lambda x: np.array(float(x)))
 
         gmm=GaussianMixture.train(df_vector,k)
         labels=gmm.predict(df_vector).collect()
 
+        labels = np.array(labels)
         n=len(labels)
         c0=len(labels[labels==0])
         c1=n-c0
@@ -65,11 +66,27 @@ def find_outliers_KGuaussians(sc, sequence,k=2,proportion=0.95):
                         return []
         else:
                 return []
+def find_outliers_Gaussian(sequence,distance_factor=3):
+        df=sequence
+        df_vector=df.map(lambda x: np.array(float(x)))
 
-def find_outliers(sc, sequence,k=2,proportion=0.95):
-        l1=find_outliers_KMeans(sc,sequence,k,proportion)
-        l2=find_outliers_KGuaussians(sc,sequence,k,proportion)
-        return l1+l2
+        gmm=GaussianMixture.train(df_vector,1)
+
+        mu, sigma=list(zip(*[(g.mu, g.sigma) for g in gmm.gaussians]))
+
+        m=mu[0].values
+        s=sqrt(sigma[0].values)
+
+        l=np.array(df_vector.collect())
+        d=abs(l-mu)
+        return d[d>=distance_factor*s]
+
+def find_outliers(sequence, k=2,proportion=0.95,distance_factor=3):
+        #l1=find_outliers_KMeans(sequence,k,proportion)
+        l2=find_outliers_KGuaussians(sequence,k,proportion)
+        l3=find_outliers_Gaussian(sequence,distance_factor)
+        return l2
+
 
         
 if __name__=='__main__':
