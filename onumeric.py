0e4228f352cac32cf8c48778897ee34fac37c1bb
yasskad <31750396+yasskad@users.noreply.github.com>
2018-04-15 23:03:11 -0400

    onumeric Kmeans updated

--- onumeric.py
+++ onumeric.py
@@ -16,20 +16,23 @@ def find_outliers_KMeans(sequence,k=2,proportion=0.95):
         clusters=KMeans.train(df_vector, k, maxIterations=10,initializationMode="random")
         center=clusters.centers
 
-        l=np.array(df_vector.collect())
-        l0=abs(l-center[0])
-        l1=abs(l-center[1])
-
-        n=len(l0)
-        c0=sum(l1-l0>=0)
-        c1=n-c0
-
-        if c0/n>proportion:
-                return list(l[l1<l0])
-        elif c1/n>proportion:
-                return list(l[l1>=l0])
-        else:
+        if len(center)==1:
                 return []
+        else:
+                l=np.array(df_vector.collect())
+                l0=abs(l-center[0])
+                l1=abs(l-center[1])
+
+                n=len(l0)
+                c0=sum(l1-l0>=0)
+                c1=n-c0
+
+                if c0/n>proportion:
+                        return list(l[l1<l0])
+                elif c1/n>proportion:
+                        return list(l[l1>=l0])
+                else:
+                        return []
 
 def find_outliers_KGuaussians(sequence,k=2,proportion=0.95,distance_factor=3):
         #currently please take input as a list and return a list
@@ -82,7 +85,7 @@ def find_outliers_Gaussian(sequence,distance_factor=3):
         return list(d[d>=distance_factor*s])
 
 def find_outliers(sequence, k=2,proportion=0.95,distance_factor=3):
-        #l1=find_outliers_KMeans(sequence,k,proportion)
+        l1=find_outliers_KMeans(sequence,k,proportion)
         l2=find_outliers_KGuaussians(sequence,k,proportion)
         l3=find_outliers_Gaussian(sequence,distance_factor)
         return l2+l3
