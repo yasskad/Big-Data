6e75db2861570e9358466b7b3ec88e7c85fa3322
raviatnyu <rtg267@nyu.edu>
2018-05-05 21:05:48 -0400

    Regex Matching For Dates

--- utils.py
+++ utils.py
@@ -16,9 +16,26 @@ def isphonenumber(x):
 	pattern = ''
 	return bool(re.match(pattern, x))
 	
-def isdate(x):
-	pattern = ''
-	return bool(re.match(pattern, x))
+def isdate(x, value=False):
+	x = re.split('T| ',x)[0]
+	delimiter = '([- /.:])'
+	year = '((19|20)\d\d)'
+	month = '(0[1-9]|1[012])'
+	date = '(0[1-9]|[12][0-9]|3[01])'
+	pattern1 = year + delimiter + month + delimiter + date
+	pattern2 =  month + delimiter + date + delimiter + year
+	pattern3 =  date + delimiter + month + delimiter + year	
+	patternlist = [pattern1, pattern2, pattern3]
+	for pattern in patternlist:
+		if bool(re.match(pattern, x)):
+			if value == False:
+				return True
+			else:
+				if pattern == pattern1:
+					return x[:4]
+				else:
+					return x[-4:]
+	return False
 
 def isstring(x):
 	pattern = '^[a-zA-z .,;:]+$'
@@ -29,6 +46,8 @@ def gettype(seq):
 	if sum(string) > len(seq)*0.75: return 'String' 
 	numeric = [isfloat(x) for x in seq]
 	if sum(numeric) > len(seq)*0.75: return 'Numeric'
+	#date = [isdate(x) for x in seq]
+	#if sum(date) > len(seq)*0.75: return 'Date' 
 	return 'None'
 		
 
@@ -48,3 +67,6 @@ if __name__=='__main__':
 	valuelist = ['22','djkas da','NY','dsa', 'a']
 	valuelist = list(filter((lambda x: isfloat(x)), valuelist))
 	print(valuelist)
+
+	print(isdate('1992:12:09', value=True))
+	print(isdate('2015-07-09T00:00:00', value=True))
